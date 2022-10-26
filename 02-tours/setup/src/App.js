180b40761bbe0cd9aad40b5cd6e2f79dacdddee2
kdang1928 <kimdang@ucsb.edu>
2022-10-26 14:03:46 -0700

    Completed Tours mini-project

--- 02-tours/setup/src/App.js
+++ 02-tours/setup/src/App.js
@@ -1,11 +1,62 @@
-import React, { useState, useEffect } from 'react'
-import Loading from './Loading'
-import Tours from './Tours'
+import React, { useState, useEffect, createContext } from "react";
+import Loading from "./Loading";
+import Tours from "./Tours";
 // ATTENTION!!!!!!!!!!
 // I SWITCHED TO PERMANENT DOMAIN
-const url = 'https://course-api.com/react-tours-project'
+const url = "https://course-api.com/react-tours-project";
+
+export const TourContext = createContext();
+
 function App() {
-  return <h2>Tours Project Setup</h2>
+  const [loading, setLoading] = useState(true);
+  const [tours, setTours] = useState([]);
+
+  const fetchTours = async () => {
+    setLoading(true);
+    const resp = await fetch(url);
+    const data = await resp.json();
+    console.log(data);
+
+    setTours(data);
+    setLoading(false);
+  };
+
+  useEffect(() => {
+    fetchTours();
+  }, []);
+
+  if (loading) {
+    return (
+      <main>
+        <Loading />
+      </main>
+    );
+  }
+
+  if (tours.length === 0) {
+    return (
+      <main>
+        <div>
+          <h2>No Tours Left</h2>
+          <button
+            onClick={() => {
+              fetchTours();
+            }}
+          >
+            Refresh Tours
+          </button>
+        </div>
+      </main>
+    );
+  }
+
+  return (
+    <main>
+      <TourContext.Provider value={[tours, setTours]}>
+        <Tours tours={tours} />
+      </TourContext.Provider>
+    </main>
+  );
 }
 
-export default App
+export default App;
--- 02-tours/setup/src/Tour.js
+++ 02-tours/setup/src/Tour.js
@@ -1,7 +1,41 @@
-import React, { useState } from 'react';
+import React, { useContext, useState } from "react";
+import { TourContext } from "./App";
 
-const Tour = () => {
-  return <h2>tour component</h2>;
+const Tour = ({ id, image, info, name, price }) => {
+  const [tours, setTours] = useContext(TourContext);
+  const [readMore, setReadMore] = useState(false);
+
+  const handleNotInterested = (id) => {
+    // console.log("Button press");
+    const newTours = tours.filter((tour) => tour.id !== id);
+    setTours(newTours);
+  };
+
+  return (
+    <article className="single-tour">
+      <img src={image} alt={name} />
+      <footer>
+        <div className="tour-info">
+          <h4>{name}</h4>
+          <h4 className="tour-price">{price}</h4>
+        </div>
+        <p>
+          {readMore ? info : `${info.substring(0, 200)}...`}
+          <button onClick={() => setReadMore(!readMore)}>
+            {readMore ? "Show Less" : "Read More"}
+          </button>
+        </p>
+
+        <button
+          className="delete-btn"
+          onClick={() => {
+            handleNotInterested(id);
+          }}
+        >
+          Not Interested
+        </button>
+      </footer>
+    </article>
+  );
 };
-
 export default Tour;
