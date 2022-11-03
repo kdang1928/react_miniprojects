7452cb4e4dcf9c8d8f0959700d540bfde26ffaef
kdang1928 <kimdang@ucsb.edu>
2022-11-03 14:12:02 -0700

    Improved Category Selection, Conditional ClassName

--- 05-menu/setup/src/Categories.js
+++ 05-menu/setup/src/Categories.js
@@ -1,7 +1,48 @@
-import React from 'react';
+import React, { useState } from "react";
+import items from "./data";
 
-const Categories = () => {
-  return <h2>categories component</h2>;
+const Categories = ({ setItems }) => {
+  const [selected, setSelected] = useState("");
+  return (
+    <div className="btn-container">
+      <button
+        className={`filter-btn ${selected || "selected"}`}
+        onClick={() => {
+          setItems(items);
+          setSelected("");
+        }}
+      >
+        All
+      </button>
+      <button
+        className={`filter-btn ${selected === "breakfast" && "selected"}`}
+        onClick={() => {
+          setItems(items.filter((item) => item.category === "breakfast"));
+          setSelected("breakfast");
+        }}
+      >
+        Breakfast
+      </button>
+      <button
+        className={`filter-btn ${selected === "lunch" && "selected"}`}
+        onClick={() => {
+          setItems(items.filter((item) => item.category === "lunch"));
+          setSelected("lunch");
+        }}
+      >
+        Lunch
+      </button>
+      <button
+        className={`filter-btn ${selected === "shakes" && "selected"}`}
+        onClick={() => {
+          setItems(items.filter((item) => item.category === "shakes"));
+          setSelected("shakes");
+        }}
+      >
+        Shakes
+      </button>
+    </div>
+  );
 };
 
 export default Categories;
