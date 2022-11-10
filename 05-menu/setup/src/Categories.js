a058c2e3e3608d59ede8fd1d60a9eacefc911d9e
kdang1928 <kimdang@ucsb.edu>
2022-11-10 13:09:39 -0800

    Dynamic category component

--- 05-menu/setup/src/Categories.js
+++ 05-menu/setup/src/Categories.js
@@ -2,10 +2,28 @@ import React, { useState } from "react";
 import items from "./data";
 
 const Categories = ({ setItems }) => {
-  const [selected, setSelected] = useState("");
+  const [selected, setSelected] = useState("all");
+  const categories = ["all", ...new Set(items.map((item) => item.category))];
+
   return (
     <div className="btn-container">
-      <button
+      {categories.map((category) => {
+        return (
+          <button
+            type="button"
+            className={`filter-btn ${selected === category && "selected"}`}
+            onClick={() => {
+              if (category !== "all")
+                setItems(items.filter((item) => item.category === category));
+              else setItems(items);
+              setSelected(category);
+            }}
+          >
+            {category}
+          </button>
+        );
+      })}
+      {/* <button
         className={`filter-btn ${selected || "selected"}`}
         onClick={() => {
           setItems(items);
@@ -40,7 +58,7 @@ const Categories = ({ setItems }) => {
         }}
       >
         Shakes
-      </button>
+      </button> */}
     </div>
   );
 };
