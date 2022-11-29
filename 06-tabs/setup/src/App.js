78ee48c79507d0582274b9f6de81cf1fbb1b0653
kdang1928 <kimdang@ucsb.edu>
2022-11-29 12:10:30 -0800

    Start of 06

--- 06-tabs/setup/src/App.js
+++ 06-tabs/setup/src/App.js
@@ -1,10 +1,29 @@
-import React, { useState, useEffect } from 'react'
-import { FaAngleDoubleRight } from 'react-icons/fa'
+import React, { useState, useEffect } from "react";
+import { FaAngleDoubleRight } from "react-icons/fa";
+import Loading from "./Loading";
 // ATTENTION!!!!!!!!!!
 // I SWITCHED TO PERMANENT DOMAIN
-const url = 'https://course-api.com/react-tabs-project'
+const url = "https://course-api.com/react-tabs-project";
 function App() {
-  return <h2>tabs project setup</h2>
+  const [loading, setLoading] = useState(false);
+  const [jobs, setJobs] = useState([]);
+
+  useEffect(() => {
+    fetchJobs();
+  }, []);
+
+  const fetchJobs = async () => {
+    setLoading(true);
+    const resp = await fetch(url);
+    const newJobs = await resp.json();
+    setLoading(false);
+    setJobs(newJobs);
+    console.log(jobs);
+  };
+
+  if (loading) return <Loading />;
+
+  return <h2>Tabs Project Setup</h2>;
 }
 
-export default App
+export default App;
